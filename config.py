7941def024c043e0eb590e6876133890adcf9d69
JasperOtieno <jaotie@gmail.com>
2026-01-05 09:08:12 +0300

    updated kpis module

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ DATA_PATH = "data/life_expectancy_world_bank.csv"
 
 # Set page title and layout
 PAGE_CONFIG = {
-    "page_title": "Life Expectancy & Socio-Economic Dashboard",
+    "page_title": "Life Expectancy and Socio-Economic Dashboard",
     "layout": "wide"
 }
